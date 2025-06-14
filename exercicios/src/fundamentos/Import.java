82476e7d72c3b1ff38cce3601c90102af419f79c
Lucas Ribeiro <lucasribeiro.sec@gmail.com>
2025-06-14 15:40:58 -0300

    import

--- exercicios/src/fundamentos/Import.java
+++ exercicios/src/fundamentos/Import.java
@@ -1,7 +1,15 @@
 package fundamentos;
 
+import java.util.Date;
+
 public class Import {
 	public static void main(String[] args) {
+		
+		java.lang.String s = "Bom dia!";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
 		
 	}
 }
