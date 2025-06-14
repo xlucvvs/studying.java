5675254e4d1132ef887ebbb059143c26c58af649
Lucas Ribeiro <lucasribeiro.sec@gmail.com>
2025-06-14 15:46:08 -0300

    diferenca entre tipos primitivos e objetos

---
+++ exercicios/src/fundamentos/PrimitivoXObjeto.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class PrimitivoXObjeto {
+
+	public static void main(String[] args) {
+		
+		// Objeto (Instancia de Classe)
+		String s = new String("Oi");
+		s.toUpperCase();
+		
+		// Tipo Primitivo!
+		int a = 333;
+		System.out.println(a);
+	}
+}
