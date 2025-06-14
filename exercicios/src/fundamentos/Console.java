721cfeb5d85cb58849b27802e80a4a4e64adc950
Lucas Ribeiro <lucasribeiro.sec@gmail.com>
2025-06-14 16:00:54 -0300

    console

--- exercicios/src/fundamentos/Console.java
+++ exercicios/src/fundamentos/Console.java
@@ -19,17 +19,17 @@ public class Console {
 		
 		Scanner entrada = new Scanner(System.in);
 		
-		System.out.print("Digite o seu nome: ");
-		String nome = entrada.nextLine();
-		
-		System.out.print("Digite o seu sobrenome: ");
-		String sobrenome = entrada.nextLine();
-
-		System.out.print("Digite a sua idade: ");
-		int idade = entrada.nextInt();
-		
-		System.out.printf("%s %s tem %d anos.%n",
-				nome, sobrenome, idade);
+			System.out.print("Digite o seu nome: ");
+			String nome = entrada.nextLine();
+			
+			System.out.print("Digite o seu sobrenome: ");
+			String sobrenome = entrada.nextLine();
+	
+			System.out.print("Digite a sua idade: ");
+			int idade = entrada.nextInt();
+			
+			System.out.printf("%s %s tem %d anos.%n",
+					nome, sobrenome, idade);
 		
 		entrada.close();
 	}
